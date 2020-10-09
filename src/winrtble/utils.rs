abe4cf06cc3d30936708e1093c2da3324007cbfe
MattesWhite <dagda92@googlemail.com>
2020-10-09 08:44:22 +0200

    Remove winrt::utils::test::check_address()
    Is replaced by general tests on BDAddr.

--- src/winrtble/utils.rs
+++ src/winrtble/utils.rs
@@ -62,37 +62,8 @@ pub fn to_char_props(_: &GattCharacteristicProperties) -> CharPropFlags {
     CharPropFlags::from_bits_truncate(0 as u8)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn check_address() {
-        let bluetooth_address = 252566450624623;
-        let addr = bluetooth_address.into();
-        let result = addr.into();
-        assert_eq!(bluetooth_address, result);
-    }
-
-    #[test]
-    fn check_uuid_to_guid_conversion() {
-        let uuid_str = "10B201FF-5B3B-45A1-9508-CF3EFCD7BBAF";
-        let uuid = Uuid::from_str(uuid_str).unwrap();
-
-        let guid_converted = to_guid(&uuid);
-
-        let guid_expected = Guid::from(uuid_str);
-        assert_eq!(guid_converted, guid_expected);
-    }
-
-    #[test]
-    fn check_guid_to_uuid_conversion() {
-        let uuid_str = "10B201FF-5B3B-45A1-9508-CF3EFCD7BBAF";
-        let guid = Guid::from(uuid_str);
-
-        let uuid_converted = to_uuid(&guid);
-
-        let uuid_expected = Uuid::from_str(uuid_str).unwrap();
-        assert_eq!(uuid_converted, uuid_expected);
-    }
-}
+// }
